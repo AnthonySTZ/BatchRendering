2fa1b3d3b03065228167a231c8ba6de85cf3c0dc
AnthonySTZ <anthonycandy2309@gmail.com>
2023-09-26 23:05:42 +0200

    Selection row new variable

--- scripts/index.js
+++ scripts/index.js
@@ -19,398 +19,11 @@ const passesTable = document.querySelector("#passesTable");
 const taskTable = document.querySelector("#taskTable");
 const slavesTable = document.querySelector("#slavesTable");
 
-const allTables = [plansTable, passesTable, taskTable, slavesTable];
-
-
-
-// Color Even Row 
-function colorTd(nodes, color){
-
-    for(let j = 0; j < nodes.length; j++){
-                
-        if (nodes[j].nodeName.toLowerCase() === "td"){
-
-            nodes[j].style.background = color; 
-
-        } 
-
-    }
-
-}
-
-
-function colorEvenRow(table){
-
-    for (let i=2; i < table.rows.length; i++){
-
-        let nodes = table.rows[i].childNodes;
-
-        if (i % 2 === 1){
-            
-            colorTd(nodes, "#292929");
-            
-        } else {
-
-            colorTd(nodes, "#1a1a1a");
-
-        }
-
-    }
-
-}
-
-// Clear All Tables
-
-
-// Create rowsList for Passes Table
-let baseColorValues = [
-    ["Name", "BaseColor.bc"],
-    ["Status", "Rendering"],
-    ["Progress", "80% (80/100)"],
-    ["Frames", "1-100"]
-];
-
-let depthValues = [
-    ["Name", "depth.z"],
-    ["Status", "Completed"],
-    ["Progress", "100% (100/100)"],
-    ["Frames", "1-100"]
-];
-
-let passesList = [
-    new Map(baseColorValues),
-    new Map(depthValues)
-];
-
-// Create rowsList for Plans Table
-let plan1Values = [
-    ["Software", "Maya"],
-    ["Path", "Test_plan1"]
-];
-
-let plan2Values = [
-    ["Software", "Maya"],
-    ["Path", "Gros"]
-];
-
-let plan3Values = [
-    ["Software", "Maya"],
-    ["Path", "CavaOuQuoi"]
-];
-
-let plansList = [
-    new Map(plan1Values),
-    new Map(plan2Values),
-    new Map(plan3Values)
-];
-
-// Create rowsList for Tasks Table
-let task1Values = [
-    ["Name", "BattleOfMonCul.baseColor"],
-    ["Frames", "1-100"],
-    ["Status", "Completed"]
-];
-
-let task2Values = [
-    ["Name", "BattleOfMonCul.depth.z"],
-    ["Frames", "1-100"],
-    ["Status", "Rendering"]
-];
-
-let task3Values = [
-    ["Name", "BattleOfMonCul.chaipa"],
-    ["Frames", "1-100"],
-    ["Status", "Queued"]
-];
-
-
-let tasksList = [
-    new Map(task1Values),
-    new Map(task2Values),
-    new Map(task3Values)
-];
-
-// Create rowsList for Slaves Table
-let slaves1Values = [
-    ["Machine", "Machine 1"],
-    ["Frames", "None"],
-    ["Status", "Offline"]
-];
-
-let slaves2Values = [
-    ["Machine", "Machine 2"],
-    ["Frames", "None"],
-    ["Status", "Offline"]
-];
-
-let slaves3Values = [
-    ["Machine", "Machine 3"],
-    ["Frames", "1-100 (50%)"],
-    ["Status", "Rendering"]
-];
-
-
-let slavesList = [
-    new Map(slaves1Values),
-    new Map(slaves2Values),
-    new Map(slaves3Values)
-];
-
-let allLists = [plansList, passesList, tasksList, slavesList];
-
-// Create Rows
-function createRow(propertiesMap){
-    
-    let row = document.createElement("tr");
-    
-    for (let [key, value] of propertiesMap){
-        
-        let cell = document.createElement("td");
-        cell.innerText = value;
-        row.appendChild(cell);
-        row.appendChild(document.createElement("td")); //Cellule vide
-        
-    }
-    
-    return row;
-    
-}
-
-function createRows(table, rowsList){
-
-    if (rowsList.length > 0){ //Check if table is empty
-
-        let row;
-
-        for (let i of rowsList){
-
-            row = createRow(i);
-            table.appendChild(row);
-
-        }
-
-    }    
-
-}
-
-function createAllRows(tables, lists){
-
-    for(let i = 0; i<tables.length; i++){
-
-        createRows(tables[i], lists[i]);
-
-    }
-
-
-}
-
-
-//createAllRows(allTables, allLists);
-
-
-// Color Row by Status
-function colorAllTexts(nodes, color){
-
-    for(let j = 0; j < nodes.length; j++){
-                
-        if (nodes[j].nodeName.toLowerCase() === "td"){
-
-            nodes[j].style.color = color; 
-
-        } 
-
-    }
-
-}
-
-function colorByStatus(table, rowList){
-
-    let status;
-    let nodes;
-
-    for (let i = 0; i < rowList.length; i++){
-
-        status = rowList[i].get("Status");
-        nodes = table.rows[i+2].childNodes;
-
-        if (status === "Queued"){
-
-            colorAllTexts(nodes, "#939393"); //Grey
-            
-        } else if (status === "Rendering"){
-
-            colorAllTexts(nodes, "#EE9616"); //Orange         
-            
-        } else if (status === "Completed"){
-
-            colorAllTexts(nodes, "#95ff8f"); //Green
-
-        } else {
-
-            colorAllTexts(nodes, "#E74D31") //Red
-
-        }
-
-
-    }
-
-
-}
-
-function colorAllTables(tables, lists){
-
-    for (let i = 0; i<tables.length; i++){
-
-        if (lists[i].length > 0){//Check if table is empty
-
-            if (lists[i][0].get("Status") != undefined){
-                colorByStatus(tables[i], lists[i]);
-            }
-            colorEvenRow(tables[i]);
-            
-        }
-
-    }
-
-
-}
-
-function createTablesDatas(lists){ //Create Datas to save
-
-    let datas = "";
-    for (let list of lists){
-
-        for(let map of list){
-
-            datas += JSON.stringify(Array.from(map.entries())) + "*";
-
-        }
-
-        datas = datas.slice(0, -1);
-
-        datas += "|";
-
-    }
-
-    return datas.slice(0, -1); //remove last "*|"
-
-}
-
-function getDatasFromSave(saveText){
-
-    const allLists = saveText.split("|");
-    let AllDatas = [];
-
-    for (let list of allLists){
-        
-        const row = list.split("*");
-        let mapList = [];
-
-        for (let element of row){
-
-            let map = new Map(JSON.parse(element));
-            mapList.push(map);
-
-        }
-
-        AllDatas.push(mapList);
-
-    }
-    
-
-    return AllDatas;
-
-}
-
-
-
-function clearAllLists(){
-
-    plansList = [];
-    passesList = [];
-    tasksList = [];
-    slavesList = [];
-
-    allLists = [plansList, passesList, tasksList, slavesList]
-
-}
-
-
-
-
-
-
-
-
-
 
 // ------------------- REMOVE BUTTON -------------------------- //
 
 // ------------------- SELECT ROW -------------------------- //
-let plansRows = plansTable.getElementsByTagName("tr");
-let plansRowSelected;
 
-let slavesRows = slavesTable.getElementsByTagName("tr");
-let slavesRowSelected;
-
-function selectRowListener(rows, tableName){
-
-    Array.from(rows).forEach((row, index) => {
-        row.addEventListener("click", () => {
-    
-            let nodes;
-            
-            let table = row.parentNode;
-
-            for (let i = 0; i<table.rows.length; i++){ //Remove Selection of all rows
-    
-                nodes = table.rows[i].childNodes;
-    
-                for(let j = 0; j < nodes.length; j++){
-                    
-                    if (nodes[j].nodeName.toLowerCase() === "td"){
-                        
-                        nodes[j].classList.remove("selectRow");
-            
-                    }
-            
-                }
-    
-            }
-    
-    
-            if (index >= 2){
-
-                if (tableName === "plansTable"){                    
-                    plansRowSelected = index-2;
-                    console.log(plansList[index-2].get("Path"));
-                } else if (tableName === "slavesTable"){
-                    slavesRowSelected = index-2;
-                    console.log(slavesList[index-2].get("Machine"));
-                }
-    
-                nodes = row.childNodes;
-    
-                for(let j = 0; j < nodes.length; j++){ //Add Selection to the row
-                    
-                    if (nodes[j].nodeName.toLowerCase() === "td"){
-                        
-                        nodes[j].classList.add("selectRow");
-            
-                    }
-            
-                }
-                
-            }
-    
-    
-        });
-    });
-
-}
-
-selectRowListener(plansRows, "plansTable")
-selectRowListener(slavesRows, "slavesTable")
 
 plansRemoveBtn.addEventListener("click", () => { //REMOVE PLANS ROW
 
@@ -830,6 +443,9 @@ function loadData(tableObjList, data){
 }
 
 
+
+
+
 // ----------- INIT TABLES ----------- //
 
 let plansRow1Values = [
@@ -847,6 +463,7 @@ let plansRow2Obj = new Map(plansRow2Values);
 
 
 let plansTableObj = {
+    name : "Plans",
     table : plansTable,
     rows : [plansRow1Obj, plansRow2Obj],
     rowSelected : undefined
@@ -873,6 +490,7 @@ let passesRow2Obj = new Map(passesRow2Values);
 
 
 let passesTableObj = {
+    name : "Passes",
     table : passesTable,
     rows : [passesRow1Obj, passesRow2Obj],
     rowSelected : undefined
@@ -911,6 +529,7 @@ let tasksRow4Obj = new Map(tasksRow4Values);
 
 
 let taskTableObj = {
+    name : "Task",
     table : taskTable,
     rows : [tasksRow1Obj, tasksRow2Obj, tasksRow3Obj, tasksRow4Obj],
     rowSelected : undefined
@@ -928,6 +547,7 @@ let slavesRow1Obj = new Map(slavesRow1Values);
 
 
 let slavesTableObj = {
+    name : "Slaves",
     table : slavesTable,
     rows : [slavesRow1Obj],
     rowSelected : undefined
@@ -999,4 +619,76 @@ openBtn.addEventListener("click", () => { //Open Button
     
 
 });
+
+
+
+
+
+
+// ------------- ROW SELECTION ------------ //
+let plansRows = plansTable.getElementsByTagName("tr");
+let plansRowSelected;
+
+let slavesRows = slavesTable.getElementsByTagName("tr");
+let slavesRowSelected;
+
+function selectRowListener(tableObj){
+
+    let rows = tableObj.table.getElementsByTagName("tr");
+
+    Array.from(rows).forEach((row, index) => {
+        row.addEventListener("click", () => {
+    
+            let nodes;            
+            let table = row.parentNode;
+
+            for (let i = 0; i<table.rows.length; i++){ //Remove Selection of all rows
+    
+                nodes = table.rows[i].childNodes;
+    
+                for(let j = 0; j < nodes.length; j++){
+                    
+                    if (nodes[j].nodeName.toLowerCase() === "td"){
+                        
+                        nodes[j].classList.remove("selectRow");
+            
+                    }
+            
+                }
+    
+            }
+    
+    
+            if (index >= 2){
+
+                if (tableObj.name === "Plans"){                    
+                    plansRowSelected = index-2;
+                    console.log(tableObj.rows[index-2].get("Path"));
+                } else if (tableObj.name === "Slaves"){
+                    slavesRowSelected = index-2;
+                    console.log(tableObj.rows[index-2].get("Machine"));
+                }
+    
+                nodes = row.childNodes;
+    
+                for(let j = 0; j < nodes.length; j++){ //Add Selection to the row
+                    
+                    if (nodes[j].nodeName.toLowerCase() === "td"){
+                        
+                        nodes[j].classList.add("selectRow");
+            
+                    }
+            
+                }
+                
+            }
+    
+    
+        });
+    });
+
+}
+
+selectRowListener(plansTableObj, "plansTable")
+selectRowListener(slavesTableObj, "slavesTable")
 
